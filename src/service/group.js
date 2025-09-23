fd9eb134920875c2ba00bf57b72dabff529b0a21
Kajal Chopade <chopadekajal01@gmail.com>
2025-09-24 01:41:15 +0530

    Group service

--- src/service/group.js
+++ src/service/group.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import { config } from "./config";
 
-
+// Fetch groups by course_id
 export async function getGroupbycourse_name(course_id) {
-
-    const url = `${config.studentServerBaseURL}/group/groupbycourse`;
-    const body = {course_id}
-    const response = await axios.post(url,body);
-    return response.data 
-    
+  try {
+    const url = `${config.cocoServerBaseURL}/group/groupbycourse`;
+    const body = { course_id };
+    const response = await axios.post(url, body);
+    return response.data; // { status: 'success', data: [...] }
+  } catch (err) {
+    return { status: "error", error: err.message };
+  }
 }
